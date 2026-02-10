533d7543d4088cc4aba0a53509425451d5487138
Mike <mike714896@gmail.com>
2026-02-10 18:19:14 -0500

    Fix webpack error

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        net: false,
-        tls: false,
-        fs: false,
-        perf_hooks: false,
-      };
-    }
-    return config;
-  },
   serverExternalPackages: ['postgres'],
 };
 
