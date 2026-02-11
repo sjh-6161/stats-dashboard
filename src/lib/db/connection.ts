f43f0175f62936121bdc939bc282adcef839915a
Mike <mike714896@gmail.com>
2026-02-10 23:50:17 -0500

    Update connection logic

--- src/lib/db/connection.ts
+++ src/lib/db/connection.ts
@@ -2,7 +2,7 @@ import postgres from 'postgres'
 
 const sql = postgres({
     host: process.env.POSTGRES_HOST as string,
-    port: 5433,
+    port: parseInt(process.env.POSTGRES_PORT as string),
     database: process.env.POSTGRES_DATABASE as string,
     username: process.env.POSTGRES_USER as string,
     password: process.env.POSTGRES_PASSWORD as string
